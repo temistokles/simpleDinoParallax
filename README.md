24a151c39161234bd76b0df47782a7801ebf14b8
Temistokles <temistokles@gmailcom>
2013-06-09 18:50:10 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,38 @@
+# Simple Dino Parallax
+
+Simple and light-weight **parallax** with support of vertical and horizontal scrolling, transparency and rotation.
+Depends on jQuery.
+
+Plugin was inspired by lack of support for various type of parallax/scroll-based plugins available.
+It is minimalistic just to fulfill the needs of http:///www.simpledino.com/. Feel free to contribute.
+
+
+## Install and usage
+Copy simpleDinoParallax.js to your project and call
+
+```
+$('.parallax').parallax();
+```
+
+(In this case, mark the required elements with class "parallax").
+
+
+To specify required effects use the following CSS classes.
+
+| Class                      | Meaning                                     |
+| -------------------------- | ------------------------------------------- |
+| parallax-vertical-scroll   | vertical parallax effect                    |  
+| parallax-hotizontal-scroll | horizontal parallax effect                  |  
+| parallax-rotate            | rotation effect based on current scroll     |  
+| parallax-transparency      | transparency effect based od current scroll |
+
+To specify speed of the effect use the following HTML5 data-* attributes.
+
+| Attribute                        | Meaning                                                                                        |
+| -------------------------------- | ---------------------------------------------------------------------------------------------- |
+| data-parallax-speed              | Speed of (any) parallax effect. Will be overriden by following attributes if they are present. |  
+| data-parallax-vertical-speed     | Speed of vertical parallax scroll effect.                                                      |  
+| data-parallax-horizontal-speed   | Speed of horizontal parallax scroll effect.                                                    |  
+| data-parallax-transparency-speed | Speed of transparency parallax effect.                                                         |  
+| data-parallax-rotate-speed       | Speed of parallax rotation effect.                                                             |
+   
---
+++ simpleDinoParallax.js
@@ -0,0 +1,88 @@
+/**
+ * Simple Dino Parallax
+ * Supports: vertical and horizontal parallaxes, transparency and rotation
+ * Source: https://github.com/temistokles/simpleDinoParallax
+ */
+(function ( $ ) {
+
+    var elements = [];
+
+    function initElement(el){
+        var position = $(el).position();
+
+        $(el).data('parallax-original-offset-top', position.top);
+        $(el).data('parallax-original-offset-left', position.left);
+
+        if ($(el).data('parallax-min-opacity')) {
+            $(el).fadeTo(0, $(el).data('parallax-min-opacity'));
+        }
+    }
+    
+    function parallaxEffect(el, scrollAmount){
+        //vertical scrolling
+        if ($(el).hasClass('parallax-vertical-scroll')) {
+            var speed = $(el).data('parallax-vertical-speed') ? $(el).data('parallax-vertical-speed') : $(el).data('parallax-speed'); 
+            var top = $(el).data('parallax-original-offset-top') + $(el).height() * speed * scrollAmount;
+            var maxTop = $(el).parent().height() - $(el).height();
+
+            if (top > maxTop){
+                top = maxTop;
+            }
+
+            $(el).css('top', top + 'px');
+        }
+
+        //horizontal scrolling
+        if ($(el).hasClass('parallax-horizontal-scroll')) {
+            var speed = $(el).data('parallax-horizontal-speed') ? $(el).data('parallax-horizontal-speed') : $(el).data('parallax-speed'); 
+            var left = $(el).data('parallax-original-offset-left') + $(el).width() * speed * scrollAmount;
+            var maxLeft = $(el).parent().width() - $(el).width();
+
+            if (left > maxLeft){
+                left = maxLeft;
+            }
+
+            var right = $(el).parent().width() - left - $(el).width();
+            $(el).css('right', right + 'px');
+        }
+
+        //transparency
+        if ($(el).hasClass('parallax-transparency')) {
+            var minOpacity = $(el).data('parallax-min-opacity');
+            var speed = $(el).data('parallax-transparency-speed') ? $(el).data('parallax-transparency-speed') : $(el).data('parallax-speed'); 
+            var opacity = scrollAmount * speed;
+            if (opacity < minOpacity) {
+                opacity = minOpacity;
+            }           
+            $(el).fadeTo(0.5, opacity);
+        }
+
+        //rotation
+        if ($(el).hasClass('parallax-rotate')) {
+            var speed = $(el).data('parallax-rotate-speed') ? $(el).data('parallax-rotate-speed') : $(el).data('parallax-speed'); 
+            var angle = 360 * scrollAmount * speed;
+
+            $(el).css('-ms-transform', 'rotate(' + angle + 'deg)')
+                    .css('-webkit-transform', 'rotate(' + angle + 'deg)')
+                    .css('-moz-transform', 'rotate(' + angle + 'deg)')
+                    .css('-o-transform', 'rotate(' + angle + 'deg)')
+                    .css('transform', 'rotate(' + angle + 'deg)');                                   
+        }
+        
+    }
+        
+    $(window).scroll(function(e){
+        var scrollAmount = $(window).scrollTop() / $(window).height();
+        for (i in elements) {
+            parallaxEffect(elements[i], scrollAmount);
+        }
+    });
+    
+    $.fn.parallax = function() {        
+        return this.each(function() {
+            initElement(this);
+            elements[elements.length] = this;
+        });        
+    };
+ 
+}( jQuery ));
